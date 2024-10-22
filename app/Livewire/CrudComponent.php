a0bd6eb6fa38d9d79825836addc65682e8f51c65
emon2021 <mdemonahmed2021@gmail.com>
2024-10-22 16:37:10 +0600

    learning laravel livewire

---
+++ app/Livewire/CrudComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CrudComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.crud-component');
+    }
+}
